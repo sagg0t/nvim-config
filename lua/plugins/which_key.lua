5e08b567870585e169ec9388bcd996b4104adaed
Oleh Volynets <saggot91@gmail.com>
2023-05-10 17:12:09 +0200

    Return of whick-key

---
+++ lua/plugins/which_key.lua
@@ -0,0 +1,46 @@
+return {
+    {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+
+            local wk = require("which-key")
+
+            wk.setup({})
+
+            wk.register(
+                {
+                    l = {
+                        name = "LSP",
+                        d = "Goto definition",
+                        D = "Goto declaration",
+                        i = "Goto implementation",
+                        f = "Formating",
+                        h = "Hover/doc",
+                        s = "Signature",
+                        r = "Rename",
+                        e = "Diagnostics pop-up",
+                    },
+                    f = {
+                        name = "Telescope",
+                        f = "find file",
+                        t = "find text",
+                        s = "document symbols",
+                        o = "recently opened files",
+                        h = "vim help",
+                        k = "keymaps",
+                        O = "vim options",
+                        ["/"] = "current buffer fuzzy search",
+                        ["?"] = "search history",
+                        [";"] = "command-line history",
+                        c = "execute command",
+                    }
+                },
+                {
+                    prefix = "<space>"
+                }
+            )
+        end,
+    }
+}
