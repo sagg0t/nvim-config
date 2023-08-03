665d3d70d5765b81f4d5aad27fecf89c970c1446
Oleh Volynets <saggot91@gmail.com>
2023-08-03 11:53:24 +0200

    Disabled tab competions

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -54,27 +54,27 @@ return {
 
                 sorting = {
                     comparators = {
-                        -- cmp.config.compare.offset,
-                        -- cmp.config.compare.exact,
-                        -- cmp.config.compare.score,
-                        --
-                        -- function(entry1, entry2)
-                        --     local _, entry1_under = entry1.completion_item.label:find "^_+"
-                        --     local _, entry2_under = entry2.completion_item.label:find "^_+"
-                        --     entry1_under = entry1_under or 0
-                        --     entry2_under = entry2_under or 0
-                        --
-                        --     if entry1_under > entry2_under then
-                        --         return false
-                        --     elseif entry1_under < entry2_under then
-                        --         return true
-                        --     end
-                        -- end,
-                        --
-                        -- cmp.config.compare.kind,
-                        -- -- cmp.config.compare.sort_text,
-                        -- cmp.config.compare.length,
-                        -- cmp.config.compare.order,
+                        cmp.config.compare.offset,
+                        cmp.config.compare.exact,
+                        cmp.config.compare.score,
+
+                        function(entry1, entry2)
+                            local _, entry1_under = entry1.completion_item.label:find "^_+"
+                            local _, entry2_under = entry2.completion_item.label:find "^_+"
+                            entry1_under = entry1_under or 0
+                            entry2_under = entry2_under or 0
+
+                            if entry1_under > entry2_under then
+                                return false
+                            elseif entry1_under < entry2_under then
+                                return true
+                            end
+                        end,
+
+                        cmp.config.compare.kind,
+                        -- cmp.config.compare.sort_text,
+                        cmp.config.compare.length,
+                        cmp.config.compare.order,
                     },
                 },
 
@@ -93,27 +93,30 @@ return {
                     })
                 },
 
-                mapping = vim.tbl_extend('force', cmp.mapping.preset.insert(), {
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
-                        -- they way you will only jump inside the snippet region
-                        if luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        elseif has_words_before() then
-                            cmp.complete()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                }),
+                mapping = cmp.mapping.preset.insert(),
+                -- mapping = vim.tbl_extend('force', cmp.mapping.preset.insert(), {
+                --     ["<Tab>"] = cmp.mapping(function(fallback)
+                --         if cmp.visible() then
+                --             cmp.select_next_item()
+                --         elseif luasnip.expand_or_locally_jumpable() then
+                --             luasnip.expand_or_jump()
+                --         elseif has_words_before() then
+                --             cmp.complete()
+                --         else
+                --             fallback()
+                --         end
+                --     end, { "i", "s" }),
+                --
+                --     ["<S-Tab>"] = cmp.mapping(function(fallback)
+                --         if cmp.visible() then
+                --             cmp.select_prev_item()
+                --         elseif luasnip.jumpable(-1) then
+                --             luasnip.jump(-1)
+                --         else
+                --             fallback()
+                --         end
+                --     end, { "i", "s" }),
+                -- }),
 
                 experimental = {
                     ghost_text = true
