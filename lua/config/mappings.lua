226fab9f3f4dc5912258299ea770e0901fc3d872
Oleh Volynets <saggot91@gmail.com>
2023-09-13 11:28:21 +0200

    Enabled netrw mappings and settings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,16 +1,14 @@
 local default_opts = { noremap = true, silent = true }
--- unmap SPACE from Leader
--- vim.keymap.set('n', '<Space>', '<NOP>', default_opts)
 
 vim.keymap.set("n", "<Leader>h", ":noh<CR>", default_opts)
 
--- vim.keymap.set("n", "<C-b><C-b>",   "<CMD>Explore<CR>",     default_opts)
--- vim.keymap.set("n", "<C-b>b",       "<CMD>Explore<CR>",     default_opts)
--- vim.keymap.set("n", "<C-b><C-s>",   "<CMD>Sexplore<CR>",    default_opts)
--- vim.keymap.set("n", "<C-b>s",       "<CMD>Sexplore<CR>",    default_opts)
--- vim.keymap.set("n", "<C-b><C-v>",   "<CMD>Vexplore<CR>",    default_opts)
--- vim.keymap.set("n", "<C-b>v",       "<CMD>Vexplore<CR>",    default_opts)
--- vim.keymap.set("n", "<C-b><C-t>",   "<CMD>Texplore<CR>",    default_opts)
--- vim.keymap.set("n", "<C-b>t",       "<CMD>Texplore<CR>",    default_opts)
+vim.keymap.set("n", "<C-b><C-b>",   "<CMD>Explore<CR>",     default_opts)
+vim.keymap.set("n", "<C-b>b",       "<CMD>Explore<CR>",     default_opts)
+vim.keymap.set("n", "<C-b><C-s>",   "<CMD>Sexplore<CR>",    default_opts)
+vim.keymap.set("n", "<C-b>s",       "<CMD>Sexplore<CR>",    default_opts)
+vim.keymap.set("n", "<C-b><C-v>",   "<CMD>Vexplore<CR>",    default_opts)
+vim.keymap.set("n", "<C-b>v",       "<CMD>Vexplore<CR>",    default_opts)
+vim.keymap.set("n", "<C-b><C-t>",   "<CMD>Texplore<CR>",    default_opts)
+vim.keymap.set("n", "<C-b>t",       "<CMD>Texplore<CR>",    default_opts)
 
 return {}
--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -59,12 +59,10 @@ opt.clipboard = 'unnamedplus'
 --     verthoriz = '╋',
 -- }
 
-vim.g.netrw_keepdir = 0
-vim.g.netrw_banner = 0
+-- vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 30
-vim.g.netrw_liststyle = 3
+vim.g.netrw_liststyle = 0
 vim.g.netrw_list_hide = "\\(^\\|\\s\\s\\)\\zs\\.\\S\\+"
-vim.g.netrw_localcopydircmd = "cp -r"
 
 
 return {}
