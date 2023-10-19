72f8093bab55d59e6c0a21bd8a3048462b7bfd5a
Oleh Volynets <saggot91@gmail.com>
2023-10-19 21:20:15 +0200

    Updated netrw settings

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -62,7 +62,20 @@ opt.clipboard = 'unnamedplus'
 -- vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 30
 vim.g.netrw_liststyle = 0
+-- Needed to be able to move files. Otherwise when not in the directory where vim was opened, can't move
+-- files. Maybe it tries to run the command with paths relevant to the current netrw dir, but in the root
+-- dir.
+vim.g.netrw_keepdir = 0 -- keep the current directory the same as the browsing directory.
 vim.g.netrw_list_hide = "\\(^\\|\\s\\s\\)\\zs\\.\\S\\+"
+-- Enable recursive copy of directories in *nix systems
+vim.g.netrw_localcopydircmd = "cp -r"
+
+-- Enable recursive creation of directories in *nix systems
+vim.g.netrw_localmkdir = "mkdir -p"
+
+-- Enable recursive removal of directories in *nix systems
+-- NOTE: we use 'rm' instead of 'rmdir' (default) to be able to remove non-empty directories
+vim.g.netrw_localrmdir = "rm -r"
 
 
 return {}
