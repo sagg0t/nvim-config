b95b0afb40407b2c40cdcb2ebecbf173a8e77d31
Oleh Volynets <saggot91@gmail.com>
2024-05-21 12:17:04 +0200

    add null_ls plugins

--- lua/plugins/lsp/null_ls.lua
+++ lua/plugins/lsp/null_ls.lua
@@ -9,8 +9,10 @@ return {
             sources = {
                 null_ls.builtins.code_actions.gomodifytags,
                 null_ls.builtins.code_actions.impl,
+                null_ls.builtins.code_actions.refactoring,
 
                 null_ls.builtins.diagnostics.buf,
+                null_ls.builtins.diagnostics.staticcheck,
 
                 null_ls.builtins.formatting.gofumpt,
                 null_ls.builtins.formatting.goimports_reviser,
