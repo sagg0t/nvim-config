b625f878383f561966709f3e4bbafe728f3d26e8
Oleh Volynets <saggot91@gmail.com>
2024-11-28 17:07:34 +0100

    feat: update netrw config

--- plugin/settings.lua
+++ plugin/settings.lua
@@ -31,8 +31,9 @@ opt.hlsearch = true
 opt.showcmd = true
 
 opt.scrolloff = 10
-opt.linebreak = true
-opt.textwidth = 80
+-- opt.linebreak = true
+-- opt.textwidth = 80
+opt.wrap = false
 opt.ruler = false
 opt.foldmethod = "indent"
 opt.foldenable = false -- unfold everything by default
@@ -60,13 +61,14 @@ opt.smartcase = true
 
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 30
-vim.g.netrw_liststyle = 0
+vim.g.netrw_liststyle = 1
+vim.g.netrw_sort_by = "extern"
 -- Needed to be able to move files. Otherwise when not in the directory where vim was opened, can"t move
 -- files. Maybe it tries to run the command with paths relevant to the current netrw dir, but in the root
 -- dir.
 -- vim.g.netrw_keepdir = 0 -- keep the current directory the same as the browsing directory.
 vim.g.netrw_keepdir = 1
--- vim.g.netrw_list_hide = "\\(^\\|\\s\\s\\)\\zs\\.\\S\\+"
+vim.g.netrw_list_hide = "\\(^\\|\\s\\s\\)\\zs\\.\\S\\+"
 --     .. ",.\\+_templ\\.\\(go\\|txt\\)"
     -- .. vim.fn["netrw_gitignore#Hide"]()
 -- Enable recursive copy of directories in *nix systems
