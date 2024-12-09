668b4a66a6b4275577e5eedc66d8864be4a02e1a
Oleh Volynets <saggot91@gmail.com>
2024-12-09 23:17:55 +0100

    feat: update Lua keymaps

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,2 +1,3 @@
-vim.keymap.set("n", "<Leader>x", ":so<CR>", { buffer = 0 })
+vim.keymap.set("n", "<Leader>x", "<cmd>.lua<CR>", { buffer = 0 })
+vim.keymap.set("n", "<Leader><Leader>x", "<cmd>so %<CR>", { buffer = 0 })
 vim.keymap.set("n", "<Leader>X", ":fclose<CR>", { buffer = 0 })
