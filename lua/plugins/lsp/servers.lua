b6bf8ddd358c30d49a707dd726b7cd1c92692692
Oleh Volynets <saggot91@gmail.com>
2024-12-11 22:26:22 +0100

    feat: minor lua_ls config update

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -84,15 +84,7 @@ return {
     lua_ls = {
         settings = {
             Lua = {
-                runtime = {
-                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                    version = 'LuaJIT',
-                    -- Setup your lua path
-                    -- local runtime_path = vim.split(package.path, ';')
-                    -- table.insert(runtime_path, "lua/?.lua")
-                    -- table.insert(runtime_path, "lua/?/init.lua")
-                    -- path = runtime_path,
-                },
+                runtime = { version = 'LuaJIT' },
                 diagnostics = {
                     -- Get the language server to recognize the `vim` global
                     globals = { 'vim' },
