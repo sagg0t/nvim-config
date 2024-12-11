7294573e68737a60ad6ed389e86692cf164255e0
Oleh Volynets <saggot91@gmail.com>
2024-12-11 09:01:16 +0100

    chore: comment out colorschemes to remove noise

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,65 +22,65 @@ return {
         end
     },
 
-{
-        "folke/tokyonight.nvim",
-        enabled = false,
-        dependencies = { "lukas-reineke/indent-blankline.nvim" },
-        lazy = false,
-        config = function()
-            vim.g.tokyonight_style = "night"
-            vim.g.tokyonight_dark_sidebar = true
-            vim.g.tokyonight_dark_float = true
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     enabled = false,
+    --     dependencies = { "lukas-reineke/indent-blankline.nvim" },
+    --     lazy = false,
+    --     config = function()
+    --         vim.g.tokyonight_style = "night"
+    --         vim.g.tokyonight_dark_sidebar = true
+    --         vim.g.tokyonight_dark_float = true
+    --
+    --         -- vim.cmd("colorscheme tokyonight")
+    --     end
+    -- },
 
-            -- vim.cmd("colorscheme tokyonight")
-        end
-    },
+    -- {
+    --     "catppuccin/nvim",
+    --     enabled = false,
+    --     name = "catppuccin",
+    --     lazy = false,
+    --     priority = 1000,
+    -- },
 
-    {
-        "catppuccin/nvim",
-        enabled = false,
-        name = "catppuccin",
-        lazy = false,
-        priority = 1000,
-    },
+    -- {
+    --     "Mofiqul/vscode.nvim",
+    --     enabled = false,
+    --     opts = {
+    --         transparent = false,
+    --         italic_comments = true,
+    --         disable_nvimtree_bg = true
+    --     },
+    -- },
 
-    {
-        "Mofiqul/vscode.nvim",
-        enabled = false,
-        opts = {
-            transparent = false,
-            italic_comments = true,
-            disable_nvimtree_bg = true
-        },
-    },
+    -- {
+    --     "rose-pine/neovim",
+    --     enabled = false,
+    --     name = "rose-pine",
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         require("rose-pine").setup({
+    --             disable_background = true,
+    --             disable_float_background = true,
+    --         })
+    --
+    --         vim.cmd.colorscheme("rose-pine")
+    --         vim.cmd("hi DiagnosticUnderlineInfo gui=NONE cterm=NONE")
+    --     end
+    -- },
 
-    {
-        "rose-pine/neovim",
-        enabled = false,
-        name = "rose-pine",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require("rose-pine").setup({
-                disable_background = true,
-                disable_float_background = true,
-            })
-
-            vim.cmd.colorscheme("rose-pine")
-            vim.cmd("hi DiagnosticUnderlineInfo gui=NONE cterm=NONE")
-        end
-    },
-
-    {
-        "sainnhe/gruvbox-material",
-        enabled = false,
-        config = function()
-            vim.g.gruvbox_material_background = "medium"
-            -- vim.g.gruvbox_material_transparent_background = 2
-            vim.g.gruvbox_material_enable_italic = 1
-            vim.g.gruvbox_material_enable_bold = 1
-            vim.g.gruvbox_material_diagnostic_virtual_text = "highlighted"
-            vim.cmd.colorscheme("gruvbox-material")
-        end
-    }
+    -- {
+    --     "sainnhe/gruvbox-material",
+    --     enabled = false,
+    --     config = function()
+    --         vim.g.gruvbox_material_background = "medium"
+    --         -- vim.g.gruvbox_material_transparent_background = 2
+    --         vim.g.gruvbox_material_enable_italic = 1
+    --         vim.g.gruvbox_material_enable_bold = 1
+    --         vim.g.gruvbox_material_diagnostic_virtual_text = "highlighted"
+    --         vim.cmd.colorscheme("gruvbox-material")
+    --     end
+    -- }
 }
