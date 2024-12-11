1378e7a511da581758a9d70ecf514eb8fbde9927
Oleh Volynets <saggot91@gmail.com>
2024-12-11 22:53:43 +0100

    fix: update blink.cmp config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,79 +2,68 @@ return {
     {
         "saghen/blink.cmp",
         lazy = false, -- lazy loading handled internally
-        -- dependencies = "rafamadriz/friendly-snippets",
         build = "cargo build --release",
 
         opts = {
-            highlight = {
-                -- sets the fallback highlight groups to nvim-cmp's highlight groups
-                -- useful for when your theme doesn't support blink.cmp
-                -- will be removed in a future release, assuming themes add support
-                use_nvim_cmp_as_default = true,
-            },
-
-            trigger = {
-                -- signature_help = { enabled = true },
-                completion = {
-                    keyword_range = "full"
-                }
-            },
-
-            fuzzy = {
-                use_fecency = false,
-            },
-
+            fuzzy = { use_fecency = false },
             keymap = { preset = "default" },
+            signature = { enabled = true },
+
+            completion = {
+                keyword = {
+                    range = "full",
+                },
+
+                documentation = {
+                    auto_show = true,
+                    window = {
+                        min_width = 15,
+                        max_width = 80,
+                        max_height = 40,
+                        -- 'single' | 'double' | 'rounded' | 'solid' | 'shadow' | 'padded' | 'none' | string[]
+                        border = "double",
+                    }
+                },
+
+                menu = {
+                    draw = {
+                        columns = { { "kind_icon" }, { "label" } }
+                    }
+                    -- draw = function(ctx)
+                    --     return {
+                    --         " ",
+                    --         { ctx.kind_icon, ctx.icon_gap, hl_group = "BlinkCmpKind" .. ctx.kind },
+                    --         {
+                    --           ctx.label,
+                    --           ctx.kind == "Snippet" and "~" or nil,
+                    --           fill = true,
+                    --           hl_group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
+                    --           max_width = 50,
+                    --         },
+                    --         " ",
+                    --         {
+                    --             ctx.item.source,
+                    --             hl_group = "@comment"
+                    --         },
+                    --         " ",
+                    --     }
+                    -- end
+                },
+            },
 
             sources = {
                 default = { "lsp", "path", "snippets", "buffer", "lazydev" },
+                per_filetype = { "lazydev", "lua" },
                 providers = {
                     snippets = { score_offset = -3 },
                     buffer = { score_offset = -4 },
                     lazydev = {
                         name = "LazyDev",
                         module = "lazydev.integrations.blink",
-                        fallbacks = "lsp"
+                        fallbacks = { "lsp" }
                     },
                 }
             },
-
-            windows = {
-                autocomplete = {
-                    draw = {
-                        columns = { { "kind_icon" }, { "label" } }
-                    }
-                --     draw = function(ctx)
-                --         return {
-                --             " ",
-                --             { ctx.kind_icon, ctx.icon_gap, hl_group = "BlinkCmpKind" .. ctx.kind },
-                --             {
-                --               ctx.label,
-                --               ctx.kind == "Snippet" and "~" or nil,
-                --               fill = true,
-                --               hl_group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
-                --               max_width = 50,
-                --             },
-                --             " ",
-                --             {
-                --                 ctx.item.source,
-                --                 hl_group = "@comment"
-                --             },
-                --             " ",
-                --         }
-                --     end
-                },
-                documentation = {
-                    min_width = 15,
-                    max_width = 80,
-                    max_height = 40,
-                    -- 'single' | 'double' | 'rounded' | 'solid' | 'shadow' | 'padded' | 'none' | string[]
-                    border = "double",
-                    auto_show = true
-                },
-            },
-
-            opts_extend = { "sources.completion.enabled_providers" }
         }
     }
 }
