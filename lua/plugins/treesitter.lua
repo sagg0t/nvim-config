d3de710b574dbcebea7384fead19b59fb61a28e1
Oleh Volynets <oleh.volynets@builder.ai>
2025-01-10 15:20:05 +0100

    feat: load TS on VeryLazy

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        event = "BufReadPost",
+        event = "VeryLazy",
         opts = {
             ensure_installed = {
                 "bash",
