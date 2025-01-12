0ba08509240773b92a7045e4b310c51e694948b6
Oleh Volynets <saggot91@gmail.com>
2025-01-12 17:12:35 +0100

    fix: range codeaction

--- lua/plugins/lsp/code_actions.lua
+++ lua/plugins/lsp/code_actions.lua
@@ -5,6 +5,36 @@ local ms = vim.lsp.protocol.Methods
 
 local CodeAction = require("ui.code_actions_widget")
 
+local function range_from_selection(bufnr, mode)
+    -- TODO: Use `vim.fn.getregionpos()` instead.
+
+    -- [bufnum, lnum, col, off]; both row and column 1-indexed
+    local start = vim.fn.getpos('v')
+    local end_ = vim.fn.getpos('.')
+    local start_row = start[2]
+    local start_col = start[3]
+    local end_row = end_[2]
+    local end_col = end_[3]
+
+    -- A user can start visual selection at the end and move backwards
+    -- Normalize the range to start < end
+    if start_row == end_row and end_col < start_col then
+        end_col, start_col = start_col, end_col
+    elseif end_row < start_row then
+        start_row, end_row = end_row, start_row
+        start_col, end_col = end_col, start_col
+    end
+    if mode == 'V' then
+        start_col = 1
+        local lines = vim.api.nvim_buf_get_lines(bufnr, end_row - 1, end_row, true)
+        end_col = #lines[1]
+    end
+    return {
+        ['start'] = { start_row, start_col - 1 },
+        ['end'] = { end_row, end_col - 1 },
+    }
+end
+
 function vim.lsp.buf.code_action(opts)
     vim.validate({ options = { opts, 't', true } })
     opts = opts or {}
@@ -46,14 +76,15 @@ function vim.lsp.buf.code_action(opts)
             assert(type(opts.range) == 'table', 'code_action range must be a table')
             local start = assert(opts.range.start, 'range must have a `start` property')
             local end_ = assert(opts.range['end'], 'range must have a `end` property')
-            params = vim.lsp.util.make_given_range_params(start, end_, bufnr)
+            params = vim.lsp.util.make_given_range_params(start, end_, bufnr, client.offset_encoding)
         elseif mode == 'v' or mode == 'V' then
-            local region = vim.region(bufnr, "'<", "'>", "b", true)
-            local range_start = { region[1] + 1, region[1][1] }
-            local range_end = { region[-1] + 1, region[-1][2] }
-
-            params =
-                vim.lsp.util.make_given_range_params(range_start, range_end, bufnr)
+            local range = range_from_selection(bufnr, mode)
+            params = vim.lsp.util.make_given_range_params(
+                range.start,
+                range['end'],
+                bufnr,
+                client.offset_encoding
+            )
         else
             params = vim.lsp.util.make_range_params(win)
         end
