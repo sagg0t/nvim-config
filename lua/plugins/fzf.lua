e153354825152d63c67358774ba58c3fa6d638b2
Oleh Volynets <saggot91@gmail.com>
2025-02-21 15:51:48 +0100

    feat: use FZF ui.select

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -71,6 +71,11 @@ return {
                     ["ctrl-q"] = "select-all+accept",
                 },
             },
-        }
+        },
+        config = function(_, opts)
+            local fzf = require("fzf-lua")
+            fzf.setup(opts)
+            fzf.register_ui_select()
+        end
     }
 }
