7c7e61264d99f6ef9dc88460c8b60a28e9011d51
Oleh Volynets <saggot91@gmail.com>
2025-02-21 15:52:35 +0100

    fix: error code duplication in virtual_lines

--- plugin/settings.lua
+++ plugin/settings.lua
@@ -92,14 +92,17 @@ end
 
 vim.diagnostic.config({
     virtual_lines = {
-        current_line = true
+        current_line = true,
+        format = function(d)
+            return d.message
+        end
     },
     virtual_text = {
         source = "if_many",
         format = function(d)
-            local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
-            if d.lnum == lnum then return nil end
-
+            --     local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
+            --     if d.lnum == lnum then return nil end
+            --
             return d.message
         end
     },
