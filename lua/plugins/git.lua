79a60751504943acb68875d29015912713780526
Oleh Volynets <oleh.volynets@builder.ai>
2025-02-24 15:27:25 +0100

    feat: enable git sign col

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -6,19 +6,16 @@ return {
         cmd = "Gitsigns",
         dependencies = { "nvim-lua/plenary.nvim" },
         keys = {
-            vim.keymap.set('n', '<leader>gb', function() require("gitsigns").blame_line{full=true} end),
-            vim.keymap.set('n', '<leader>gh', function() require("gitsigns").stage_hunk() end),
-            vim.keymap.set('n', '<leader>gH', function() require("gitsigns").undo_stage_hunk() end),
-            vim.keymap.set('n', '<leader>gd', function() require("gitsigns").diffthis() end),
-            vim.keymap.set('n', '<leader>gst', function() require("gitsigns").toggle_signs() end),
+            vim.keymap.set("n", "<leader>gb", function() require("gitsigns").blame_line({ full = true }) end),
+            vim.keymap.set("n", "<leader>gh", function() require("gitsigns").stage_hunk() end),
+            vim.keymap.set("n", "<leader>gH", function() require("gitsigns").undo_stage_hunk() end),
+            vim.keymap.set("n", "<leader>gd", function() require("gitsigns").diffthis() end),
+            vim.keymap.set("n", "<leader>gst", function() require("gitsigns").toggle_signs() end),
         },
         opts = {
-            signcolumn = false,
+            signcolumn = true,
             current_line_blame = true,
-            current_line_blame_opts = {
-                delay = 1000,
-                virt_text_pos = 'eol'
-            },
+            current_line_blame_opts = { delay = 1000, virt_text_pos = "eol" },
             current_line_blame_formatter = "<author>, <author_time:%R>",
         }
     }
