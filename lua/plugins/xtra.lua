94aa0118d9e4d6e33cac441b1d5fc6a10cc5ce53
Oleh Volynets <saggot91@gmail.com>
2025-02-24 08:54:44 +0100

    chore: merge plugin files

--- lua/plugins/xtra.lua
+++ lua/plugins/xtra.lua
@@ -17,7 +17,7 @@ return {
     {
         "nvimdev/indentmini.nvim",
         event = "VeryLazy",
-        commit = "f0839ba",
+        dir = "~/devel/indentmini.nvim",
         opts = {}
     },
 
@@ -69,5 +69,35 @@ return {
             enable_tailwind = true,
             render = "foreground"
         }
-    }
+    },
+
+    {
+        "kylechui/nvim-surround",
+        opts = {}
+    },
+
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {},
+    },
+
+    {
+        "Wansmer/treesj",
+        lazy = true,
+        keys = {
+            { "gs", function() require("treesj").toggle() end },
+            { "gS", function() require("treesj").toggle({ split = { recursive = true } }) end },
+        },
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = {
+            use_default_keymaps = false,
+            max_join_length = 1000
+        }
+    },
+
+    {
+        "ThePrimeagen/git-worktree.nvim",
+        lazy = true
+    },
 }
