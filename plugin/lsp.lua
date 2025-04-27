471300fbd32a203ffbc3604bae3ba62d99828abe
Oleh Volynets <saggot91@gmail.com>
2025-04-27 13:24:56 +0200

    refactor: LSP attach for Ruby

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -51,62 +51,20 @@ vim.api.nvim_create_autocmd("LspAttach", {
         map("gO", function() require("fzf-lua").lsp_document_symbols() end, "Document Symbols")
         map("<leader>lf", vim.lsp.buf.format, "Format document")
 
-        if client:supports_method("textDocument/formatting", 0) then
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = pre_write_group,
-                buffer = event.buf,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = event.buf, id = client.id })
-                end
-            })
-        end
+        -- if client:supports_method("textDocument/formatting", 0) then
+        --     vim.api.nvim_create_autocmd("BufWritePre", {
+        --         group = pre_write_group,
+        --         buffer = event.buf,
+        --         callback = function()
+        --             vim.lsp.buf.format({ bufnr = event.buf, id = client.id })
+        --         end
+        --     })
+        -- end
 
         if client:supports_method("textDocument/foldingRange", 0) then
             local win = vim.api.nvim_get_current_win()
             vim.wo[win][0].foldmethod = "expr"
             vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
-        end
-
-        if client.name == "ruby_lsp" then
-            local registered_commands = vim.api.nvim_get_commands({ builtin = false })
-            if not registered_commands["Gems"] then
-                vim.api.nvim_create_user_command(
-                    "Gems",
-                    function()
-                        local params = vim.lsp.util.make_text_document_params()
-
-                        client.request("rubyLsp/workspace/dependencies", params, function(error, gems)
-                            if error then
-                                print("Error showing deps: " .. error)
-                                return
-                            end
-
-                            -- item format {
-                            --   dependency = false, -- direct or indirect dependency
-                            --   name = "rake",
-                            --   path = "<path to gem>
-                            --   version = "13.2.1"
-                            -- }
-
-                            vim.ui.select(
-                                gems,
-                                {
-                                    prompt = "Select a gem:",
-                                    format_item = function(item)
-                                        return string.format("%s (%s)", item.name, item.version)
-                                    end
-                                },
-                                function(selection)
-                                    if not selection then return end
-
-                                    vim.cmd(string.format("split %s", selection.path))
-                                end
-                            )
-                        end)
-                    end,
-                    {}
-                )
-            end
         end
     end
 })
