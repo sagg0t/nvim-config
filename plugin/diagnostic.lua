4b2b67117e838d50f551aa9d891f4e26d6c9a964
Oleh Volynets <saggot91@gmail.com>
2025-05-15 10:22:26 +0200

    update diagnostic sign definition format

--- plugin/diagnostic.lua
+++ plugin/diagnostic.lua
@@ -1,10 +1,3 @@
--- local diagnostics_icons = { Error = "󰃤", Warn = "", Info = "󰍩", Hint = "" }
-local diagnostics_icons = { Error = "󰅚", Warn = "󰀪", Info = "󰋽", Hint = "󰌶" }
-for type, icon in pairs(diagnostics_icons) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
 vim.diagnostic.config({
     virtual_lines = {
         current_line = true,
@@ -23,4 +16,24 @@ vim.diagnostic.config({
     },
     float = { source = true },
     underline = true,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚",
+            [vim.diagnostic.severity.WARN] = "󰀪",
+            [vim.diagnostic.severity.INFO] = "󰋽",
+            [vim.diagnostic.severity.HINT] = "󰌶"
+        }
+    }
 })
+-- text = {
+--     [vim.diagnostic.severity.ERROR] = "󰅚",
+--     [vim.diagnostic.severity.WARN] = "󰀪",
+--     [vim.diagnostic.severity.INFO] = "󰋽",
+--     [vim.diagnostic.severity.HINT] = "󰌶"
+-- }
+-- text = {
+--     [vim.diagnostic.severity.ERROR] = "󰃤",
+--     [vim.diagnostic.severity.WARN] = "",
+--     [vim.diagnostic.severity.INFO] = "󰍩",
+--     [vim.diagnostic.severity.HINT] = ""
+-- }
