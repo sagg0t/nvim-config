a3200f2f514ffa3174bc4a37def3a56bd0b25f10
Oleh Volynets <saggot91@gmail.com>
2025-06-01 09:27:58 +0200

    move to main branch treesitter, install supported parsers on demand

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,95 +1,30 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        version = false,
+        lazy = false,
+        branch = "main",
         build = ":TSUpdate",
-        event = { "VeryLazy" },
-        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-        opts = {
-            ensure_installed = {
-                "bash",
-                "c",
-                "cmake",
-                "cpp",
-                "css",
-                "cuda",
-                "dockerfile",
-                "fish",
-                "git_config",
-                "git_rebase",
-                "gitattributes",
-                "gitcommit",
-                "gitignore",
-                "glsl",
-                "go",
-                "gomod",
-                "gosum",
-                "gotmpl",
-                "gowork",
-                "gpg",
-                "graphql",
-                "haskell",
-                "haskell_persistent",
-                "hcl",
-                "hlsl",
-                "html",
-                "http",
-                "ini",
-                "java",
-                "javascript",
-                "jq",
-                "jsdoc",
-                "json",
-                "latex",
-                "llvm",
-                "lua",
-                "luadoc",
-                "make",
-                "markdown",
-                "markdown_inline",
-                "ninja",
-                "odin",
-                "passwd",
-                "php",
-                "phpdoc",
-                "proto",
-                "python",
-                "query",
-                "regex",
-                "ruby",
-                "rust",
-                "scheme",
-                "scss",
-                "sql",
-                "ssh_config",
-                "swift",
-                "terraform",
-                "toml",
-                "tsx",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "xml",
-                "yaml",
-                "zig",
-            },
+        opts = {},
+        config = function(opts)
+            local ts = require("nvim-treesitter")
 
-            synct_install = false,
-            auto_install = true,
+            ts.setup(opts)
 
-            indent = { enable = false },
-            highlight = {
-                enable = true, -- false will disable the whole extension
-                disable = {},  -- list of language that will be disabled
-                -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-                --  If you are experiencing weird indenting issues, add the language to
-                --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-                -- additional_vim_regex_highlighting = { 'ruby' },
-            },
-        },
-        config = function(_, opts)
-            require('nvim-treesitter.install').prefer_git = true
-            require("nvim-treesitter.configs").setup(opts)
+            local supported_fts = ts.get_available()
+            local ft_group = vim.api.nvim_create_augroup("sagg0t FileType", { clear = true })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                group = ft_group,
+                pattern = supported_fts,
+                callback = function(evt)
+                    local ok, _ = pcall(vim.treesitter.start, evt.buf)
+                    if not ok then
+                        vim.notify("TS parser for " .. evt.match .. " is missing, installing...", vim.log.levels.WARN)
+                        ts.install(evt.match):wait(120000)
+                        pcall(vim.treesitter.start, evt.buf)
+                    end
+                end
+            })
         end
     },
 }
