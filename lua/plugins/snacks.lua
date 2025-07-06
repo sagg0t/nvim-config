cf8e9c6b3ae628829e0256cd9607d701cb66251a
Oleh Volynets <saggot91@gmail.com>
2025-07-06 16:48:44 +0200

    remove git integrations

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -14,7 +14,6 @@ return {
                 desc = "highlight groups",
             },
             { "<Leader>fk", function() Snacks.picker.keymaps() end,         desc = "keymaps" },
-            -- { "<Leader>fO", function() Snacks.picker.nvim_options() end,    desc = "vim options" },
             { "<Leader>f/", function() Snacks.picker.lines() end,           desc = "buf fuzzy find" },
             { "<Leader>f?", function() Snacks.picker.search_history() end,  desc = "search history" },
             { "<Leader>f;", function() Snacks.picker.command_history() end, desc = "command history" },
@@ -26,7 +25,7 @@ return {
                 layout = {
                     preset = "ivy_split",
                 }
-            }
+            },
         }
     }
 }
