4ee98811f06b80756d946f9a6871a037d4b08a7f
Oleh Volynets <saggot91@gmail.com>
2025-08-27 13:53:47 +0200

    drop none-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,22 +7,4 @@ return {
     },
 
     { "neovim/nvim-lspconfig" },
-
-    {
-        "nvimtools/none-ls.nvim",
-        lazy = true,
-        ft = { "go" },
-        config = function()
-            local null_ls = require("null-ls")
-
-            null_ls.setup({
-                sources = {
-                    null_ls.builtins.code_actions.gomodifytags,
-                    null_ls.builtins.code_actions.impl,
-
-                    null_ls.builtins.formatting.goimports_reviser,
-                },
-            })
-        end
-    }
 }
