67edae22cc28a711b7508f25f61c3cf1955d2a8c
Oleh Volynets <saggot91@gmail.com>
2026-03-12 22:42:39 +0100

    fix(lua): exec mappings

--- after/ftplugin/lua/mappings.lua
+++ after/ftplugin/lua/mappings.lua
@@ -1,3 +1,4 @@
-vim.keymap.set("n", "<Leader>x", "<cmd>.lua<CR>", { buffer = 0 })
-vim.keymap.set("n", "<Leader><Leader>x", "<cmd>so %<CR>", { buffer = 0 })
+vim.keymap.set("n", "<Leader>x", ":.lua<CR>", { buffer = 0 })
+vim.keymap.set("v", "<Leader>x", ":lua<CR>", { buffer = 0 })
+vim.keymap.set("n", "<Leader><Leader>x", ":so %<CR>", { buffer = 0 })
 vim.keymap.set("n", "<Leader>X", ":fclose<CR>", { buffer = 0 })
