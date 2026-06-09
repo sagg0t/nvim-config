402a4099161a79c1f68f1dcda7770a14abc0807c
Oleh Volynets <saggot91@gmail.com>
2026-06-09 09:44:37 +0200

    update on_yank -> hl_op

--- plugin/aucmds.lua
+++ plugin/aucmds.lua
@@ -1,31 +1,33 @@
-vim.api.nvim_create_autocmd("TextYankPost", {
-    group = vim.api.nvim_create_augroup("sagg0t.highlight_yank", { clear = true }),
+local api = vim.api
+
+api.nvim_create_autocmd({ "TextYankPost", "TextPutPost" }, {
+    group = api.nvim_create_augroup("sagg0t.highlight_yank", { clear = true }),
     pattern = "*",
     callback = function()
-        vim.hl.on_yank({ higroup = "IncSearch", timeout = 69 })
+        vim.hl.hl_op({ higroup = "IncSearch", timeout = 69 })
     end,
 })
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-    group = vim.api.nvim_create_augroup("sagg0t.remove_trailing_whitespace", { clear = true }),
+api.nvim_create_autocmd("BufWritePre", {
+    group = api.nvim_create_augroup("sagg0t.remove_trailing_whitespace", { clear = true }),
     pattern = { "rb", "ts", "tsx", "js", "jsx" },
     command = [[%s/\s\+$//e]],
     desc = "removes trailing whitespace"
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-    group = vim.api.nvim_create_augroup("sagg0t.big_file", { clear = true }),
+api.nvim_create_autocmd("FileType", {
+    group = api.nvim_create_augroup("sagg0t.big_file", { clear = true }),
     desc = "Disable features in big files",
     pattern = "bigfile",
     callback = function(args)
         vim.schedule(function()
-            vim.bo[args.buf].syntax = vim.filetype.match { buf = args.buf } or ""
+            vim.bo[args.buf].syntax = vim.filetype.match({ buf = args.buf }) or ""
         end)
     end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-    group = vim.api.nvim_create_augroup("sagg0t.close_with_q", { clear = true }),
+api.nvim_create_autocmd("FileType", {
+    group = api.nvim_create_augroup("sagg0t.close_with_q", { clear = true }),
     desc = "Close with <q>",
     pattern = { "git", "help", "man", "qf", "scratch" },
     callback = function(args)
@@ -33,29 +35,29 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
-vim.api.nvim_create_autocmd("BufReadPost", {
-    group = vim.api.nvim_create_augroup("saggot.goto_last_location", { clear = true }),
+api.nvim_create_autocmd("BufReadPost", {
+    group = api.nvim_create_augroup("saggot.goto_last_location", { clear = true }),
     desc = "Go to the last location when opening a buffer",
     callback = function(args)
-        local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
-        local line_count = vim.api.nvim_buf_line_count(args.buf)
+        local mark = api.nvim_buf_get_mark(args.buf, '"')
+        local line_count = api.nvim_buf_line_count(args.buf)
         if mark[1] > 0 and mark[1] <= line_count then
             vim.cmd('normal! g`"zz')
         end
     end,
 })
 
-local line_numbers_group = vim.api.nvim_create_augroup("sagg0t.toggle_rel_numbers", {})
-vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+local line_numbers_group = api.nvim_create_augroup("sagg0t.toggle_rel_numbers", { clear = true })
+api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
     group = line_numbers_group,
     desc = "Toggle relative line numbers on",
     callback = function()
-        if vim.wo.nu and not vim.startswith(vim.api.nvim_get_mode().mode, "i") then
+        if vim.wo.nu and not vim.startswith(api.nvim_get_mode().mode, "i") then
             vim.wo.relativenumber = true
         end
     end,
 })
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
     group = line_numbers_group,
     desc = "Toggle relative line numbers off",
     callback = function(args)
